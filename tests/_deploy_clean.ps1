b21d057b8967b7ce5294cf99fff15ef0ba582cc1
YanziWuX <yanziwu@example.com>
2026-07-16 21:05:31 +0800

    Remove tests/ from repo, ignore entire tests/ directory

--- tests/_deploy_clean.ps1
+++
@@ -1,4 +0,0 @@
-
-    Set-Location "D:\Code\YanziWu VirtualPrinter\tests"
-    & powershell -File ".\deploy_test.ps1" -Cleanup
-
--- tests/_restart_spool.ps1
+++
@@ -1,8 +0,0 @@
-
-    Remove-Item "C:\Windows\System32\mfilemon.log" -Force -ErrorAction SilentlyContinue
-    & net stop spooler /y 2>&1 | Out-Null
-    Start-Sleep 3
-    & net start spooler 2>&1 | Out-Null
-    Start-Sleep 8
-    Get-Content "C:\Windows\System32\mfilemon.log"
-
